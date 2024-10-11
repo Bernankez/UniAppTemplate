8d53a83612901430a31bbf0cea4dcf9d5f22d044
cole <bernankeic@gmail.com>
2024-10-11 10:24:11 +0800

    feat: add `usePagination` composable

--- src/auto-imports.d.ts
+++ src/auto-imports.d.ts
@@ -228,6 +228,7 @@ declare global {
   const useOffsetPagination: typeof import('@vueuse/core')['useOffsetPagination']
   const useOnline: typeof import('@vueuse/core')['useOnline']
   const usePageLeave: typeof import('@vueuse/core')['usePageLeave']
+  const usePagination: typeof import('./composables/usePagination')['usePagination']
   const useParallax: typeof import('@vueuse/core')['useParallax']
   const useParentElement: typeof import('@vueuse/core')['useParentElement']
   const usePerformanceObserver: typeof import('@vueuse/core')['usePerformanceObserver']
@@ -544,6 +545,7 @@ declare module 'vue' {
     readonly useOffsetPagination: UnwrapRef<typeof import('@vueuse/core')['useOffsetPagination']>
     readonly useOnline: UnwrapRef<typeof import('@vueuse/core')['useOnline']>
     readonly usePageLeave: UnwrapRef<typeof import('@vueuse/core')['usePageLeave']>
+    readonly usePagination: UnwrapRef<typeof import('./composables/usePagination')['usePagination']>
     readonly useParallax: UnwrapRef<typeof import('@vueuse/core')['useParallax']>
     readonly useParentElement: UnwrapRef<typeof import('@vueuse/core')['useParentElement']>
     readonly usePerformanceObserver: UnwrapRef<typeof import('@vueuse/core')['usePerformanceObserver']>
---
+++ src/composables/usePagination.ts
@@ -0,0 +1,58 @@
+import type { Awaitable } from '@vueuse/core'
+
+export interface UsePaginationOptions {
+  /** Start page index, defaults to `0` */
+  startPage?: number
+  /** Default page index, defaults to `startPage` */
+  defaultPage?: number
+  /** Default page size, defaults to `10` */
+  defaultSize?: number
+}
+
+export interface UsePaginationRequest<T> {
+  (page: number, size: number): Awaitable<{ data: T[], page?: number }>
+}
+
+export function usePagination<T>(request: UsePaginationRequest<T>, options?: UsePaginationOptions) {
+  const { startPage = 0, defaultSize = 10 } = options || {}
+  const defaultPage = options?.defaultPage ?? startPage
+
+  const page = ref(defaultPage)
+  const size = ref(defaultSize)
+  const isDone = ref(false)
+  const data = shallowRef<T[]>([])
+
+  async function _handleRequest(_page: number, _size: number) {
+    const { data: returnedData, page: returnedPage } = await request(_page, _size)
+    data.value.push(...returnedData)
+    if (returnedData.length < _size) {
+      isDone.value = true
+      return returnedData
+    }
+    if (isDefined(returnedPage)) {
+      page.value = returnedPage
+    }
+    return returnedData
+  }
+
+  async function refresh() {
+    page.value = startPage
+    size.value = defaultSize
+    await _handleRequest(page.value, size.value)
+    return data.value
+  }
+
+  async function next() {
+    page.value++
+    await _handleRequest(page.value, size.value)
+    return data.value
+  }
+
+  return {
+    page,
+    size,
+    data,
+    refresh,
+    next,
+  }
+}
