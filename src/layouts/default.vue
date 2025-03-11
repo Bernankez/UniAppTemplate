53a7b10a055255dfb760e4185183bbfa67715403
cole <bernankeic@gmail.com>
2025-03-11 14:37:27 +0800

    feat: add toast and message box slot for default layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,9 +3,15 @@ import themeVars from '@/styles/wot'
 </script>
 
 <template>
-  <wd-config-provider :theme-vars="themeVars">
-    <view class="p-sm">
+  <WdConfigProvider :theme-vars="themeVars">
+    <slot name="toast">
+      <WdToast />
+    </slot>
+    <slot name="messageBox">
+      <WdMessageBox />
+    </slot>
+    <view class="box-border p-sm pb-max-safe-0.875rem">
       <slot />
     </view>
-  </wd-config-provider>
+  </WdConfigProvider>
 </template>
